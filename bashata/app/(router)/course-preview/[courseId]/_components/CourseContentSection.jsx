92e1333143f3ff681ce5955c9d68f8c5592e8407
AlainDLC <jd_alain@hotmail.com>
2024-12-25 19:10:01 +0100

    course-preview 2:15

--- bashata/app/(router)/course-preview/[courseId]/_components/CourseContentSection.jsx
+++ bashata/app/(router)/course-preview/[courseId]/_components/CourseContentSection.jsx
@@ -1,10 +1,10 @@
+"use client";
 import { Lock } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
 function CourseContentSection({ courseInfo }) {
+  const [activeIndex, setActiveIndex] = useState(0);
   const chapter = courseInfo?.chapter;
-
-  console.log(chapter);
 
   // Om chapter är en array, mappa över den, annars visa enskilt kapitel
   if (Array.isArray(chapter) && chapter.length > 0) {
@@ -26,19 +26,19 @@ function CourseContentSection({ courseInfo }) {
       <div className="p-3 bg-white rounded-sm">
         <h2 className="text-xl font-bold mb-3">Course Content</h2>
         <div
-          className="p-2 text-[14px] flex justify-between items-center border rounded-sm px-4 cursor-pointer mt-2
-        hover:bg-gray-200 hover:text-gray-500"
+          className={`p-2 text-[14px] flex justify-between items-center border rounded-sm px-4 cursor-pointer mt-2
+        hover:bg-gray-200 hover:text-gray-500 ${
+          activeIndex === chapter.name && "bg-primary"
+        }`}
         >
-          <h2>
-            {chapter.id.length}. {chapter.name}
-          </h2>
+          {/* VISA PLAY  får gå igenom Hgraph chapter buggar */}
+          <h2>{chapter.name}</h2>
           <Lock className="h-4 w-4" />
         </div>
       </div>
     );
   }
 
-  // Om inget kapitel finns
   return <p>No chapter data available.</p>;
 }
 
